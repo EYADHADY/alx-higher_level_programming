82893e25d826aa90adaf83632eef6dc86b233749
Mohamed abdelhady Ibrahim <117633961+EYADHADY@users.noreply.github.com>
2023-07-25 14:47:44 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-if (process.argv.length > 2) {
-  request(process.argv[2], (error, response, responseBody) => {
-    const userCompletionCount = {};
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
 
-    if (error) {
-      console.log(error);
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
     }
 
-    JSON.parse(responseBody).forEach(item => {
-      if (item.completed) {
-        if (!userCompletionCount[item.userId]) {
-          userCompletionCount[item.userId] = 0;
-        }
-        userCompletionCount[item.userId]++;
-      }
-    });
-    console.log(userCompletionCount);
-  });
-}
+    console.log(completedTasksByUsers);
+  }
+})
